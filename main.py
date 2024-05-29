96649d51bf318d1aa4be733dcc0c3d14b70cb76e
ryenn890 <160070960+ryenn890@users.noreply.github.com>
2024-05-30 09:51:09 +1200

    first quiz

---
+++ main.py
@@ -0,0 +1,32 @@
+# Ask the user name and save
+name = input("what your name?")
+# greet user and intro quiz 
+print("welcome to the quiz,", name)
+print("This quiz is about football") 
+# ask the questions
+answer = input("who won the world cup last year")
+if answer == "Argentina":
+    print("cool,good jod")
+print("Last year's world cup winner was Argentina")
+
+
+
+# end the quiz
+print ("thanks,see you..")   
+
+
+if name == "spider":
+    print("that's cool.")
+    print("my name is raihan.")
+print("let's play game")
+
+run = input("do you want to run")
+
+while run == "yes":
+     print("keep running then")
+     
+
+score == 0
+play = "yes"
+
+name = input   
